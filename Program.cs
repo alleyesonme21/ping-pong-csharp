ed5a8626c5cbd67cd2850d388a038ddde9091f40
James Henager <jameschenager@gmail.com>
2020-07-13 11:57:00 -0700

    add ui logic to collect a number from the user and print it in the console
    Co-authored-by: Taylor Somers <tsomers@yandex.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace PingPong
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Enter a Whole Number:");
+      string inputNumber = Console.ReadLine();
+      int myNumber = int.Parse(inputNumber);
+      Console.WriteLine(myNumber);
     }
   }
 }
